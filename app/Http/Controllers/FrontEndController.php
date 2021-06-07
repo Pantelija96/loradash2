b61fc3b88b79f192fcdd8c08abd1c84854f101f2
PantelijaStosic <pantelijastosic@gmail.com>
2021-06-07 13:51:21 +0200

    initial commit

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontEndController extends Controller
+{
+    //
+
+    private $data = [];
+
+    function index(){
+      return view('welcome', $this->data);
+    }
+}
